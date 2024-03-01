8401d074e1e021bd4482e376657b052bdfda9eb4
shamoon1997 <sshamoon83@gmail.com>
2024-03-01 13:17:21 -0500

    chore: node version updated

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ import { kpis, products, transactions } from './data/data.js';
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
 /* ROUTES */
 app.use('/kpi', kpiRoutes);
